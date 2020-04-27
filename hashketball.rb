7a691bcd6382a9c49a1f4c865f8ea9e562706f4e
steven <bbtrombone@sbcglobal.net>
2020-04-27 08:24:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,19 @@
-# Write your code here!
+def game_hash
+{:home => {:team_name => "", 
+  :colors => [], 
+   :players => [{
+    :player_name => "",
+    :number => "",
+    :points => "",
+    :rebounds => "",
+    :assists => "",
+    :steals => "",
+    :block => "",
+    :slam_dunks => ""
+  }]},
+ :away => {:team_name => "", 
+ :colors => [], 
+ :players => [{
+   
+ }]}}
+end 
