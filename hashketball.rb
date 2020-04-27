c1f734494c4688e14fdc08536effd62c950dbff4
steven <bbtrombone@sbcglobal.net>
2020-04-27 08:30:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
   :colors => [], 
    :players => [{
     :player_name => "Alan Anderson",
-    :number => "0",
+    :number => 0,
     :shoe => "16",
     :points => "22",
     :rebounds => "12",
