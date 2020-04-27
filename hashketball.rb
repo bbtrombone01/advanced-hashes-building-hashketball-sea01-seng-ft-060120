079bf6c3544236a7359cd260882f6a641ca588f9
steven <bbtrombone@sbcglobal.net>
2020-04-27 08:38:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,8 +15,14 @@ def game_hash
   },
   {
     :player_name => "Reggie Evans",
-    :number => 2,
-  
+    :number => 30,
+    :shoe => 14,
+    :points => 12,
+    :Rebounds => 12,
+    :assists => 12,
+    :steals => 12,
+    :blocks => 12,
+    :slam_dunks => 7,
   }
   ]
   },
