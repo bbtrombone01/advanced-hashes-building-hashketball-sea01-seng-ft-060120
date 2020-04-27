b5f1dc6b12f9063017dae965f4c288511ede11e5
steven <bbtrombone@sbcglobal.net>
2020-04-27 09:11:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -96,7 +96,15 @@ def game_hash
      :slam_dunks => 5
    },
    {
-     :player_name => ""
+     :player_name => "Ben Gordon",
+     :number => 8,
+     :shoe => 15,
+     :points => 33,
+     :rebounds => 3,
+     :assists => 2,
+     :steals => 7,
+     :blocks => 5,
+     :slam_dunks => 12
    },
  ]}}
 end 
