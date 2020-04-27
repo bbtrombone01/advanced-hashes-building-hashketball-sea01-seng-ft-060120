3271a25e4341023494f259a456202095b6e236c2
steven <bbtrombone@sbcglobal.net>
2020-04-27 09:14:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -102,9 +102,9 @@ def game_hash
      :points => 33,
      :rebounds => 3,
      :assists => 2,
-     :steals => 7,
-     :blocks => 5,
-     :slam_dunks => 12
+     :steals => 1,
+     :blocks => 1,
+     :slam_dunks => 0
    },
  ]}}
 end 
